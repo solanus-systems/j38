19834c871c1b8594c7e6674e10c18a20963f987c
Nick Budak <budak@stanford.edu>
2023-07-24 13:23:01 -0700

    Initial implementation

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,75 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * j38: Forward messages from a CF Pub/Sub Broker to Analytics Engine
  */
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+import { isValidBrokerRequest, PubSubMessage } from '@cloudflare/pubsub'
+
+// The maximum number of calls to Analytics Engine we can make in a single request
+const MAX_ANALYTICS_CALLS = 25
+
+// Forward a batch of messages to Analytics Engine
+async function relay(messages: Array<PubSubMessage>, env: any, ctx: ExecutionContext): Promise<Array<PubSubMessage>> {
+	// Messages may be batched at higher throughput, so we should loop over
+	// the incoming messages and process them as needed.
+	// We take only the first MAX_ANALYTICS_CALLS messages to avoid exceeding the
+	// maximum number of calls to Analytics Engine in a single request.
+	for (let msg of messages.slice(0, MAX_ANALYTICS_CALLS)) {
+		// Destructure the topic from the message
+		// The first segment is the prefix and the final segment is the device ID
+		// All other segments as used as tags for filtering the data
+		// An example: "/dt/pepper_bridge/barrel_room/west/hT6Zd55hQo"
+		const [prefix, ...topic_segments] = msg.topic.split('/')
+		const device_id = topic_segments.pop()
+
+		// Ignore commands and other non-data messages
+		// Telemetry topics always begin with /dt, following the AWS IoT convention
+		if (prefix != 'dt') continue
+
+		// If the message was an object, use the values from it as the stored measurements
+		// Otherwise use the message payload itself as the (single) stored measurement
+		const payload = JSON.parse(msg.payload.toString())
+		const measurements = typeof payload === 'object' ? Object.values(payload) : [payload]
+
+		// Write the data to Analytics Engine
+		env.ANALYTICS.writeDataPoint({
+			indexes: [device_id],
+			blobs: topic_segments,
+			doubles: measurements,
+		})
+	}
+
+	// Return the messages unmodified
+	return messages
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
+		// Retrieve this from our Broker's "publicKey" field.
+		//
+		// Each Broker has a unique key to distinguish between our Broker vs. others
+		// We store these keys in environmental variables (/workers/configuration/environment-variables/)
+		// to avoid needing to fetch them on every request.
+		let publicKeys = env.BROKER_PUBLIC_KEYS
+
+		// Critical: we must validate the incoming request is from our Broker.
+		//
+		// In the future, Workers will be able to do this on our behalf for Workers
+		// in the same account as our Pub/Sub Broker.
+		if (await isValidBrokerRequest(request, publicKeys)) {
+			// Parse the PubSub message
+			let incomingMessages: Array<PubSubMessage> = await request.json()
+
+			// Pass the messages to our relay handler, and capture the returned
+			// message.
+			let outgoingMessages = await relay(incomingMessages, env, ctx)
+
+			// Re-serialize the messages and return a HTTP 200.
+			// The Content-Type is optional, but must either be
+			// "application/octet-stream" or left empty.
+			return new Response(JSON.stringify(outgoingMessages), { status: 200 })
+		}
+
+		// If the request is not from our Broker, return a HTTP 403 Forbidden.
+		return new Response('Missing or invalid broker credentials', { status: 403 })
 	},
-};
+}
